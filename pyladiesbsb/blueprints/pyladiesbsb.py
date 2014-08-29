302176b7fa203d60d0be7fb67aa039a64bf62f8d
kamillacrozara <holanda.kamilla@gmail.com>
2014-08-28 21:53:22 -0300

    mudando template e adicionando paginas de login e de posts

--- pyladiesbsb/blueprints/pyladiesbsb.py
+++ pyladiesbsb/blueprints/pyladiesbsb.py
@@ -11,27 +11,29 @@ pyladiesbsb_blueprint = Blueprint('pyladiesbsb', __name__)
 
 @pyladiesbsb_blueprint.route("/")
 def index():
-    noticias = get_table('noticias')
-    todas_as_noticias = noticias.all()
+    logs = get_table('logs')
+    todos_os_logs = logs.all()
     return render_template('index.html',
-                           noticias=todas_as_noticias,
+                           logs=todos_os_logs,
                            title=u"Todas as notícias")
 
 @pyladiesbsb_blueprint.route("/log")
 @pyladiesbsb_blueprint.route("/log/<int:log_id>")
 def log(log_id=0):
     if log_id:
-        logs = get_table('noticias')
+        logs = get_table('logs')
         log = logs.find_one(id=log_id)
         return render_template('log.html', log=log)
     else:
-        return render_template('log.html')
+        logs = get_table('logs')
+        todos_os_logs = logs.all()
+        return render_template('log.html',
+                           logs=todos_os_logs,
+                           title=u"Todas as notícias")
 
-
-
-@pyladiesbsb_blueprint.route("/log/cadastro", methods=["GET", "POST"])
-def loggin():
-    noticias = get_table('noticias')
+@pyladiesbsb_blueprint.route("/logging", methods=["GET", "POST"])
+def logging():
+    logs = get_table('logs')
     if request.method == "POST":
 
         dados_do_formulario = request.form.to_dict()
@@ -43,21 +45,36 @@ def loggin():
             imagem.save(path)
             dados_do_formulario['imagem'] = filename
 
-        id_nova_noticia = noticias.insert(dados_do_formulario)
+        id_nova_noticia = logs.insert(dados_do_formulario)
         return render_template('cadastro_sucesso.html',
                                id_nova_noticia=id_nova_noticia)
 
-    return render_template('log.html', title=u"Inserir nova noticia")
+    return render_template('logging.html')
 
 
-@pyladiesbsb_blueprint.route("/eventos/")
+@pyladiesbsb_blueprint.route("/signin", methods=["GET", "POST"])
+def signin():
+    logs = get_table('logs')
+    if request.method == "POST":
+
+        dados_do_formulario = request.form.to_dict()
+        imagem = request.files.get('imagem')
+
+        if imagem:
+            filename = secure_filename(imagem.filename)
+            path = os.path.join(current_app.config['MEDIA_ROOT'], filename)
+            imagem.save(path)
+            dados_do_formulario['imagem'] = filename
+
+        id_nova_noticia = logs.insert(dados_do_formulario)
+        return render_template('cadastro_sucesso.html',
+                               id_nova_noticia=id_nova_noticia)
+
+    return render_template('signin.html')
+
+@pyladiesbsb_blueprint.route("/eventos")
 def eventos():
     return render_template('eventos.html')
-
-
-@pyladiesbsb_blueprint.route("/contato/")
-def contato():
-    return render_template('contato.html')
 
 @pyladiesbsb_blueprint.route('/media/<path:filename>')
 def media(filename):
